46d8f8c22f882f08f01ad29934ebdb3b64076f65
Luis Loaeza <luis.loaeza@hotmail.com>
2021-04-23 15:08:56 -0500

    Añadido template del login

--- dashboard/coaches/views.py
+++ dashboard/coaches/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from coaches.models import Coach
+from django.template.loader import get_template
 from django.http import HttpResponse
 
-def coaches(request):
-    coaches = ['John', 'Jane']
-    return HttpResponse(str(coaches))
+class HomeView():
+
+    def login(self):
+        template = get_template('login.html')
+        return HttpResponse(template.render())
+
+    def list_coach(request):
+        coaches = Coach.objects.all()
+        return render(request,
+            'CoachList.html',
+            {'coaches':coaches})
+    
--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -2,10 +2,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
+
+from coaches.views import HomeView
 from coaches import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('coaches/', views.coaches),
+    path('', HomeView.login, name='home'),
+    path('admin/', admin.site.urls, name='admin'),
+    path('coaches/', HomeView.list_coach, name='coaches'),
+
 ]
